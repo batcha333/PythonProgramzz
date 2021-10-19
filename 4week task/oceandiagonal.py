0e5da4a14401b3c8ed93d5e3d9297aa60ba68195
batcha333 <mafktkader@gmail.com>
2021-10-19 18:40:45 +0530

    ocean new commit

--- 4week task/oceandiagonal.py
+++ 4week task/oceandiagonal.py
@@ -2,8 +2,10 @@ n="ocean"
 a=5
 for i in range(a):
     for j in range(a):
-        if(i==j or i+j==a-1):
+        if(i==j):
             print(n[i],end=" ")
+        elif(i+j==4):
+            print(n[j],end=" ")
         else:
             print(' ',end=" ")
     print()
