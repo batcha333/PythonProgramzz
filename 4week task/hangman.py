3dcef3c3781e15f832e4e3380ff73d88ec48acfd
batcha333 <mafktkader@gmail.com>
2021-10-28 18:18:13 +0530

    hangman

--- 4week task/hangman.py
+++ 4week task/hangman.py
@@ -1,3 +1,4 @@
+import random
 H = ['''
   +---+
   |   |
@@ -48,15 +49,35 @@ H = ['''
  / \  |
       |
 =========''']
-word="manga".strip()
+bucket={
+  "movies":["matrix","twilight","battleship","fast"],
+  "sport":["cricket","handball","rugby","kabaddi"]
+}
+INPUT=input("enter your choice:").lower()
+word=random.choice(bucket[INPUT])
 count=0
+l=["-" for i in word]
+reserved=[]
 end_game = True
+print(*l)
+def ans():
+  for i in range(len(word)):
+    if word[i]==inp:
+      l[i]=inp
 while end_game:
-    inp=input("enter:")
-    if inp in word:
-        print('')
-    else:
-        print(H[count])
-        count+=1
-    if(count == 7):
-        end_game = False  
+  inp=input("enter: ")
+  while inp in reserved:
+    inp=input("Invalid,give another input: ")
+  reserved.append(inp)
+  if inp in word:
+    ans()
+    print(*l)
+  else:
+    print(H[count])
+    count+=1
+  if(count == 7):
+    end_game = False  
+    print("you lose")
+  if "-" not in l:
+    print('you won')
+    break
